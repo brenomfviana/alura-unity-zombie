50e87c4ca92407e093ec2eb35b24908445708098
Breno Viana <brenomfviana@gmail.com>
2022-08-07 15:59:17 -0300

    improves player movimentation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        //
-    }
+    public float speed = 15;
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward);
+        float xAxis = Input.GetAxis("Horizontal");
+        float zAxis = Input.GetAxis("Vertical");    
+        Vector3 direction = new Vector3(xAxis, 0, zAxis);
+        transform.Translate(direction * speed * Time.deltaTime);
     }
 }
