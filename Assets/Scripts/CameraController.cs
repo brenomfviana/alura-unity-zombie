f52b4df294c357fc735897f537d1ee9629fe58e3
Breno Viana <brenomfviana@gmail.com>
2022-08-14 13:30:40 -0300

    removes comments

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,13 +7,11 @@ public class CameraController : MonoBehaviour
     public GameObject player;
     private Vector3 distance;
 
-    // Start is called before the first frame update
     void Start()
     {
         distance = transform.position - player.transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.position = distance + player.transform.position;
