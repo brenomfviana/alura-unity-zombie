af108bfba9ec448a336bcdac8d57ef4eceb24846
Breno Viana <brenomfviana@gmail.com>
2022-11-27 12:54:13 -0300

    fixes weapon shoot and destroys enemies

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Weapon : MonoBehaviour
+public class Bullet : MonoBehaviour
 {
     public float speed = 30;
 
@@ -18,5 +18,14 @@ public class Weapon : MonoBehaviour
         Vector3 position = rigidbody.position;
         Vector3 direction = transform.forward;
         rigidbody.MovePosition(position + direction * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Inimigo")
+        {
+            Destroy(other.gameObject);
+        }
+        Destroy(gameObject);
     }
 }
